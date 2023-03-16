2c400ed3d847f98c76217315ff77b1b00af805e3
olokunde stephen <olokundestephen@gmail.com>
2023-03-16 13:07:45 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0
+ * main - A program that print a line with puts function
+ * Return: 0 (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
